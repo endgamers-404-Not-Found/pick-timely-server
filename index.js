39abc8564b1f5447db1992a736a79d0b95985917
Azim Chowdhury <azimchy994@gmail.com>
2022-07-24 23:26:17 +0600

    mongodb setup and add user api

--- index.js
+++ index.js
@@ -3,15 +3,43 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.Name}:${process.env.Pass}@cluster0.sbqudjz.mongodb.net/?retryWrites=true&w=majority`; 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+ 
+try {
+    client.connect();
+    const userCollection = client.db("Pick-Timely").collection("userCollection");
+
+    //post a new user
+    app.post('/addUser',async(req,res)=>{
+        const name = req.body.name;
+        const email = req.body.email;
+        const result = await userCollection.insertOne({name,email})
+        res.send(result)
+    })
+
+} 
+catch {
+    // client.close();
+}
+
+
+
+
+//basic server
 app.get('/',(req,res)=>{
     res.send('server running')
 })
+
+
 
 app.listen(port,()=>{
     console.log(`server running on the port `,port)

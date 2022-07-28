f42ddccb54c701d2e75149745c7cdba0920d243d
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-29 01:57:42 +0600

    profile APIs are added

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
         await client.connect();
         const userCollection = client.db("Pick-Timely").collection("userCollection");
         const Packages = client.db("Pick-Timely").collection("Packages");
+        const profileCollection = client.db("Profile").collection("profileCollection")
 
         // basic server
         app.get('/', async (req, res) => {
@@ -34,16 +35,34 @@ async function run() {
             const result = await userCollection.insertOne({ name, email })
             res.send(result)
         })
- 
+
 
         //get all packages
-        app.get("/packages",async(req,res)=>{
+        app.get("/packages", async (req, res) => {
             const result = await Packages.find().toArray();
             res.send(result)
-        })
+        });
 
+        //updating users profile
+        app.put('/update/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const profile = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: profile,
+            };
+            const result = await profileCollection.updateOne(filter, updateDoc, options);
+            res.send({ success: true, result });
+        });
 
-
+         //get profile data.
+        app.get('/profile/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await profileCollection.find(query).toArray();
+            res.send(result);
+        });
     }
 
     finally {

9754abcc573d6d90a9b226e635b1bcc52716378e
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-08-12 14:35:43 +0600

    post and blog api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const app = express();
+const axios = require('axios');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -11,8 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/',(req,res)=>{
-    res.send('server running')
+app.get('/', (req, res) => {
+  res.send('server running')
 })
 
 
@@ -20,166 +21,182 @@ const uri = `mongodb+srv://${process.env.Name}:${process.env.Pass}@cluster0.sbqu
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
+  try {
 
-        await client.connect();
-        const userCollection = client.db("Pick-Timely").collection("userCollection");
-        const Packages = client.db("Pick-Timely").collection("Packages");
-        const hostCollection = client.db("Pick-Timely").collection("hoster");
-        const meetingCollection = client.db("Pick-Timely").collection("meetingSchedule");
-        const reviewCollection = client.db("Pick-Timely").collection("userReviews")
-
-
-        // basic server
-        app.get('/', async (req, res) => {
-            res.send('server running')
-        })
+    await client.connect();
+    const userCollection = client.db("Pick-Timely").collection("userCollection");
+    const Packages = client.db("Pick-Timely").collection("Packages");
+    const hostCollection = client.db("Pick-Timely").collection("hoster");
+    const meetingCollection = client.db("Pick-Timely").collection("meetingSchedule");
+    const reviewCollection = client.db("Pick-Timely").collection("userReviews")
+    const blogCollection = client.db("Pick-Timely").collection("blogs");
 
 
-        //post a new user
-        app.post('/addUser', async (req, res) => {
-            const name = req.body.name;
-            const email = req.body.email;
-            const result = await userCollection.insertOne({ name, email })
-            res.send(result)
-        })
-
-
-        //get all packages
-        app.get("/packages", async (req, res) => {
-            const result = await Packages.find().toArray();
-            res.send(result)
-
-        });
-
-        //updating users profile
-        app.put('/update/:email', async (req, res) => {
-            const email = req.params.email;
-            const filter = { email: email };
-            const profile = req.body;
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: profile,
-            };
-            const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send({ success: true, result });
-        });
-
-         //get profile data.
-        app.get('/profile/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email };
-            const result = await userCollection.findOne(query);
-            res.send(result);
-        }); 
-
-        
-
-        //get a package by id
-        app.get('/package/:id',async(req,res)=>{
-            const id=req.params.id;
-            const query = {_id:ObjectId(id)}
-            const result= await Packages.findOne(query)
-            res.send(result);
-        })
-
-
-        //create a payment intent
-        app.post('/createPaymentIntent', async (req, res) => {
-            const { price } = req.body;
-            // console.log(cost)
-            const amount = parseInt(price) * 100;
-            const paymentIntent = await stripe.paymentIntents.create({
-              amount: amount,
-              currency: 'usd',
-              payment_method_types: ['card']
-            })
-            res.send({ clientSecret: paymentIntent.client_secret })
-          })
-
-
-          //update user data after payment
-            //update after payment
-    app.put('/payment/:email', async (req, res) => {
-        const email = req.params.email;
-        const stripeReturn = req.body.paymentIntent;
-        const price = req.body.price;
-        const filter = { email:email };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: {
-            transactionId: `${stripeReturn.id}`, status: `${price ===130 ? 'corporate':'team'}`
-          }
-        };
-        const result = await userCollection.updateOne(filter, updateDoc, options);
-        res.send(result)
-      })
-
-      //get Host data
-      app.get("/hoster",async(req, res)=>{
-        const result = await hostCollection.find().toArray();
-        res.send(result)
-    });
-
-    app.post('/hoster', async (req, res)=>{
-        const newSchedule = req.body;
-        const result = await hostCollection.insertOne(newSchedule);
-        res.send(result);
-    });
-
-    app.get('/hoster/:id', async (req, res)=>{
-        const id = req.params.id;
-        const query = {_id:ObjectId(id)};
-        const result = await hostCollection.findOne(query);
-        res.send(result);
-    });
-
-    app.post('/schedule', async (req, res)=>{
-        const newSchedule = req.body;
-        const result = await meetingCollection.insertOne(newSchedule);
-        res.send(result);
-    });
-
-    app.get("/schedule", async(req, res)=>{
-        const result = await meetingCollection.find().toArray();
-        res.send(result)
-    });
-
-
-
-      // all of review api
-      // post api for review
-      app.post('/review', async (req, res) => {
-        const review = req.body;
-        const result = await reviewCollection.insertOne(review);
-        res.send(result);
-      })
-
-      app.get('/review', async (req, res) => {
-        const query = {};
-        const result = await reviewCollection.find(query).toArray();
-        res.send(result)
-      })
-
-      app.get('/customers', async (req, res) => {
-        const query = {};
-        const result = await userCollection.find(query).toArray();
-        res.send(result);
-      })
-
-    }
-
-    finally {
-        // client.close();
-    }
-
-
-
-
-
-    app.listen(port, () => {
-        console.log('server running on the port ', port);
+    // basic server
+    app.get('/', async (req, res) => {
+      res.send('server running')
     })
+
+
+    //post a new user
+    app.post('/addUser', async (req, res) => {
+      const name = req.body.name;
+      const email = req.body.email;
+      const result = await userCollection.insertOne({ name, email })
+      res.send(result)
+    })
+
+
+    //get all packages
+    app.get("/packages", async (req, res) => {
+      const result = await Packages.find().toArray();
+      res.send(result)
+
+    });
+
+    //updating users profile
+    app.put('/update/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const profile = req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: profile,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send({ success: true, result });
+    });
+
+    //get profile data.
+    app.get('/profile/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+
+
+    //get a package by id
+    app.get('/package/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const result = await Packages.findOne(query)
+      res.send(result);
+    })
+
+
+    //create a payment intent
+    app.post('/createPaymentIntent', async (req, res) => {
+      const { price } = req.body;
+      // console.log(cost)
+      const amount = parseInt(price) * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({ clientSecret: paymentIntent.client_secret })
+    })
+
+
+    //update user data after payment
+    //update after payment
+    app.put('/payment/:email', async (req, res) => {
+      const email = req.params.email;
+      const stripeReturn = req.body.paymentIntent;
+      const price = req.body.price;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          transactionId: `${stripeReturn.id}`, status: `${price === 130 ? 'corporate' : 'team'}`
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+
+    //get Host data
+    app.get("/hoster", async (req, res) => {
+      const result = await hostCollection.find().toArray();
+      res.send(result)
+    });
+
+    app.post('/hoster', async (req, res) => {
+      const newSchedule = req.body;
+      const result = await hostCollection.insertOne(newSchedule);
+      res.send(result);
+    });
+
+    app.get('/hoster/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await hostCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post('/schedule', async (req, res) => {
+      const newSchedule = req.body;
+      const result = await meetingCollection.insertOne(newSchedule);
+      res.send(result);
+    });
+
+    app.get("/schedule", async (req, res) => {
+      const result = await meetingCollection.find().toArray();
+      res.send(result)
+    });
+
+
+
+    // all of review api
+    // post api for review
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    })
+
+    app.get('/review', async (req, res) => {
+      const query = {};
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/customers', async (req, res) => {
+      const query = {};
+      const result = await userCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //Post blogs. 
+    app.post('/blog', async (req, res) => {
+      const review = req.body;
+      const result = await blogCollection.insertOne(review);
+      res.send({ success: true, result });
+    })
+
+    //Get all post.
+    app.get('/blog', async (req, res) => {
+      const query = {};
+      const result = await blogCollection.find(query).toArray();
+      res.send(result)
+    })
+
+
+  }
+
+  finally {
+    // client.close();
+  }
+
+
+
+
+
+  app.listen(port, () => {
+    console.log('server running on the port ', port);
+  })
 
 }
 

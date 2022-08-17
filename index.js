f842339fe7bc9e95062cd3575da574330da2d038
SMhamidAzad <hamidazad020@gmail.com>
2022-08-17 20:10:12 +0600

    before pull

--- index.js
+++ index.js
@@ -448,18 +448,5 @@ run().catch(console.dir)
 
 
 
-function printNumbers(from, to) {
-  let current = from;
 
-  setTimeout(function go() {
-    console.log(current);
-    if (current < to) {
-      setTimeout(go, 1000);
-    }
-    current++;
-  }, 1000);
-}
-
-// usage:
-printNumbers(5, 10);
 

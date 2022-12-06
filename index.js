37fe15ad896fae095f55f079ce06ebfdf1b1c24d
Azim Chowdhury <azimchy994@gmail.com>
2022-12-06 13:07:15 +0600

    vercel server deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 // const nodemailer = require('nodemailer');
 
-
+ 
 
 app.use(cors())
 app.use(express.json());
